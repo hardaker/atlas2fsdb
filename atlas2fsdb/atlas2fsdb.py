7ffb63a2e0c91c42ce1779198f41bf146bfeecc6
Wes Hardaker <opensource@hardakers.net>
2025-08-05 09:56:31 -0700

    package start with half-working support

---
+++ atlas2fsdb/atlas2fsdb.py
@@ -0,0 +1,132 @@
+"""Convert RIPE Atlas json files to FSDB TSV"""
+
+from __future__ import annotations
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType, Namespace
+from logging import debug, info, warning, error, critical
+import logging
+import sys
+import json
+import pyfsdb
+import dns.message
+
+# optionally use rich
+try:
+    from rich import print
+    from rich.logging import RichHandler
+    from rich.theme import Theme
+    from rich.console import Console
+except Exception:
+    debug("install rich and rich.logging for prettier results")
+
+# optionally use rich_argparse too
+help_handler = ArgumentDefaultsHelpFormatter
+try:
+    from rich_argparse import RichHelpFormatter
+
+    help_handler = RichHelpFormatter
+except Exception:
+    debug("install rich_argparse for prettier help")
+
+
+def parse_args() -> Namespace:
+    """Parse the command line arguments."""
+    parser = ArgumentParser(
+        formatter_class=help_handler,
+        description=__doc__,
+        epilog="Example Usage: atlas2fsdb input.json output.fsdb",
+    )
+
+    parser.add_argument(
+        "--log-level",
+        "--ll",
+        default="info",
+        help="Define the logging verbosity level (debug, info, warning, error, fotal, critical).",
+    )
+
+    parser.add_argument(
+        "input_file", type=FileType("r"), nargs="?", default=sys.stdin, help=""
+    )
+
+    parser.add_argument(
+        "output_file", type=FileType("w"), nargs="?", default=sys.stdout, help=""
+    )
+
+    args = parser.parse_args()
+    log_level = args.log_level.upper()
+    handlers = []
+    datefmt = None
+    messagefmt = "%(levelname)-10s:\t%(message)s"
+
+    # see if we're rich
+    try:
+        handlers.append(
+            RichHandler(
+                rich_tracebacks=True,
+                tracebacks_show_locals=True,
+                console=Console(
+                    stderr=True, theme=Theme({"logging.level.success": "green"})
+                ),
+            )
+        )
+        datefmt = " "
+        messagefmt = "%(message)s"
+    except Exception:
+        debug("failed to install RichHandler")
+
+    logging.basicConfig(
+        level=log_level, format=messagefmt, datefmt=datefmt, handlers=handlers
+    )
+    return args
+
+
+def main():
+    args = parse_args()
+    contents = json.load(args.input_file)
+
+    # a given query contains attributes that apply to
+    # a nested list of multiple results
+    outer_contents = {
+        "probe_id": "prb_id",
+        "probe_src": "from",
+        "query_type": "type",
+        "event_timestamp": "timestamp",
+        "stored_timestamp": "stored_timestamp",
+    }
+
+    # each nested result has its own parameters
+    result_contents = {
+        "timestamp": "time",
+        "dst_addr": "dst_addr",
+        "dst_port": "dst_port",
+        "address_family": "af",
+        "protocol": "proto",
+        "src_addr": "src_addr",
+    }
+
+    # and inside the result is another "result" structure with greater
+    # details
+    result_result_contents = {
+        "response_time": "rt",
+        "size": "size",
+        "ancount": "ANCOUNT",
+        "qdcount": "QDCOUNT",
+        "nscount": "NSCOUNT",
+        "arcount": "ARCOUNT",
+    }
+    column_names = list(outer_contents.keys())
+    column_names.extend(result_contents.keys())
+    column_names.extend(result_result_contents.keys())
+
+    with pyfsdb.Fsdb(
+        out_file_handle=args.output_file, out_column_names=column_names
+    ) as outh:
+        for content in contents:
+            for result in content["resultset"]:
+                row = [content.get(outer_contents[key]) for key in outer_contents]
+                row.extend([result.get(result_contents.get(key)) for key in result_contents])
+                row.extend([result.get("result", {}).get(result_result_contents[key]) for key in result_result_contents])
+                outh.append(row)
+
+
+if __name__ == "__main__":
+    main()
