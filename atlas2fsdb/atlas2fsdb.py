a659dd29ce35ba8be39330079ad49444c464fc78
Wes Hardaker <opensource@hardakers.net>
2025-08-05 10:29:50 -0700

    add some abuf results

--- atlas2fsdb/atlas2fsdb.py
+++ atlas2fsdb/atlas2fsdb.py
@@ -8,6 +8,7 @@ import sys
 import json
 import pyfsdb
 import dns.message
+import base64
 
 # optionally use rich
 try:
@@ -113,9 +114,17 @@ def main():
         "nscount": "NSCOUNT",
         "arcount": "ARCOUNT",
     }
+
+    dns_abuf_contents = {
+        "opcode",
+        "rcode",
+        "nsid",
+    }
+
     column_names = list(outer_contents.keys())
     column_names.extend(result_contents.keys())
     column_names.extend(result_result_contents.keys())
+    column_names.extend(dns_abuf_contents)
 
     with pyfsdb.Fsdb(
         out_file_handle=args.output_file, out_column_names=column_names
@@ -124,7 +133,44 @@ def main():
             for result in content["resultset"]:
                 row = [content.get(outer_contents[key]) for key in outer_contents]
                 row.extend([result.get(result_contents.get(key)) for key in result_contents])
-                row.extend([result.get("result", {}).get(result_result_contents[key]) for key in result_result_contents])
+
+                if "result" in result:
+                    row.extend([result["result"].get(result_result_contents[key]) for key in result_result_contents])
+
+                    if "abuf" in result["result"]:
+                        try:
+                            dnsmsg = dns.message.from_wire(base64.b64decode(result["result"]["abuf"]))
+
+                            row.append(dnsmsg.opcode().name)
+                            row.append(dnsmsg.rcode().name)
+
+                            # get the NSID of the server's response
+                            nsid_option = dnsmsg.get_options(dns.edns.NSID)
+                            if nsid_option and len(nsid_option) > 0:
+                                # TODO(hardaker): what is len > 1 mean?
+                                text = nsid_option[0].to_text()
+                                if text.startswith("NSID "):
+                                    text = text[5:]
+                                row.append(text)
+                            else:
+                                row.append(None)
+
+                        except:
+                            row.extend([None for x in dns_abuf_contents])
+
+                        # TODO(hardaker): iterate over rrset...
+                        # TODO(hardaker): iterate over extended_errors
+                        # TODO(hardaker): iterate over options
+                        # TODO(hardaker): many other dns.message.QueryMessage fields
+
+                    else:
+                        row.extend([None for x in dns_abuf_contents])
+
+                else:
+                    # fill missing data with Nones
+                    row.extend([None for x in result_result_contents])
+                    row.extend([None for x in dns_abuf_contents])
+
                 outh.append(row)
 
 
